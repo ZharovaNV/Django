ffa19437508c9dcc1770948e4a76f3ddd8af7c9a
ZharovaNV <ZharovaNV@yandex.ru>
2019-01-24 03:25:09 +0300

    Домашнее задание к уроку 8

--- server/main/static/main/products.js
+++ server/main/static/main/products.js
@@ -1,6 +1,6 @@
-const renderBook = ({id, url, sauthor, sname}) => (
+const renderBook = ({id, image, sauthor, sname}) => (
     `
-    <a href="/catalog/${id}/"><img class="imgcatalog" src="${url}" alt="${sauthor}"></a>
+    <a href="/catalog/${id}/"><img class="imgcatalog" src="${image}" alt="${sauthor}"></a>
 
 
     <div class="catalogBook">
--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'accounts',
     'utility',
     'images',
@@ -55,6 +56,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 3
+}
 
 ROOT_URLCONF = 'server.urls'
 
